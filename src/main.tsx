958c4677d34136972cc43d3171fde7ca983cb503
Dmitry Suslov <dmitrysuslov04@gmail.com>
2024-12-09 22:49:05 +0300

    gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+// import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css'; // Импортируем Provider
+// import { store } from './store'; // Импортируем ваш store
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <BrowserRouter basename="my-app">
+    // <Provider store={store}> {/* Оборачиваем Provider */}
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+        basename="/WATER-COUNTERS"
+      >
         <App />
-    </BrowserRouter>
-)
+      </BrowserRouter>
+    // </Provider>
+  );
+  
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+      navigator.serviceWorker
+        .register('/WATER-COUNTERS/serviceWorker.js')
+        .then((res) => console.log('Service Worker registered', res))
+        .catch((err) => console.log('Service Worker not registered', err));
+    });
+  }
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//     <BrowserRouter basename="my-app">
+//         <App />
+//     </BrowserRouter>
+// )
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,23 +1,23 @@
-import { Container, Row, CardImg, CardImgOverlay, CardTitle, CardText, Card, CardBody } from "reactstrap";
-import { useState } from "react";
+import { CardImg, CardTitle, CardText, Card, CardBody } from "reactstrap";
+// import { useState } from "react";
 import './index.css'; // Импортируем стили
-const items = [
-    {
-        src: "http://127.0.0.1:9000/images/mai.jpg", // Замените URL на актуальный
-        altText: "Первый технический университет в России",
-        caption: "Прообраз современного вуза, основан при Екатерине II"
-    },
-    {
-        src: "http://127.0.0.1:9000/images/main.jpg", // Замените URL на актуальный
-        altText: "Здание выдержано в стиле ампира",
-        caption: "Здание университета построено в стиле позднего московского ампира"
-    },
-    {
-        src: "http://127.0.0.1:9000/images/main.jpg", // Замените URL на актуальный
-        altText: "Инновации и технологии",
-        caption: "МГТУ им. Баумана — ведущий технический вуз в России, задающий тренды в науке и технологиях"
-    }
-];
+// const items = [
+//     {
+//         src: "http://127.0.0.1:9000/images/mai.jpg", // Замените URL на актуальный
+//         altText: "Первый технический университет в России",
+//         caption: "Прообраз современного вуза, основан при Екатерине II"
+//     },
+//     {
+//         src: "http://127.0.0.1:9000/images/main.jpg", // Замените URL на актуальный
+//         altText: "Здание выдержано в стиле ампира",
+//         caption: "Здание университета построено в стиле позднего московского ампира"
+//     },
+//     {
+//         src: "http://127.0.0.1:9000/images/main.jpg", // Замените URL на актуальный
+//         altText: "Инновации и технологии",
+//         caption: "МГТУ им. Баумана — ведущий технический вуз в России, задающий тренды в науке и технологиях"
+//     }
+// ];
 
 const item = {
     src: "http://127.0.0.1:9000/images/main.jpg", // Замените URL на актуальный
@@ -26,8 +26,8 @@ const item = {
 }
 
 const HomePage = () => {
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [animating, setAnimating] = useState(false);
+    // const [activeIndex, setActiveIndex] = useState(0);
+    // const [animating, setAnimating] = useState(false);
 
     return (
         <Card className="card-custom" color="light" outline>
